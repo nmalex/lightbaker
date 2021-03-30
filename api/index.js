76877600a91615ab5edb526be26030adf7ab6e9f
ymysevych <ymysevych@gmail.com>
2021-03-30 21:22:22 +0300

    removed unused chunks

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const upload = multer({
 });
 const fs = require('fs');
 const path = require('path');
-const axios = require("axios");
 const { uuid } = require('uuidv4');
 
 const { Pool } = require("pg");
@@ -29,70 +28,6 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-async function doconvert(stpFileUrl) {
-  const apiKey = "75f5-4d53-b0f4";
-  const workspaceGuid = "55a0bd33-9f15-4bc0-a482-17899eb67af3";
-  const baseUrl = "https://api2.renderfarmjs.com/v1";
-  const workgroup = "dev";
-  
-  var response = await axios.post(baseUrl + '/session', {
-    api_key: apiKey,
-    workgroup: workgroup,
-    workspace_guid: workspaceGuid,
-    scene_filename: null,
-    //
-    debug: additonalParams && additonalParams.debug,
-  });
-  const session = response.data.data;
-  console.log(` >> session: `, session);
-
-
-  var response2 = await axios.post(baseUrl + '/job/convert', {
-    session_guid: session.guid,
-    job_type: "convert",
-    input_url: stpFileUrl,
-    settings: {}
-  }, {
-    timeout: 15 * 60 * 1000, // 15min timeout
-  });
-
-  const job = response2.data.data;
-  console.log(" >> job: ", job);
-
-  let i = 50;
-  let timer;
-  var p = new Promise(function (resolve, reject) {
-
-    timer = setInterval(async function () {
-      try {
-        let url = baseUrl + '/job/' + job.guid;
-        console.log(" >> GET job: ", url);
-        var response = await axios.get(url, {});
-        console.log(" >> job: ", response.data.data);
-        if (response.data.data.closed) {
-          resolve();
-        }
-      } catch (err) {
-        console.log(" >> job err: ", err.message);
-        reject();
-      }
-
-      i = i - 1;
-      if (i <= 0) resolve();
-
-    }, 1000);
-
-  });
-
-  await p;
-
-  var response3 = await axios.delete(baseUrl + '/session/' + session.guid, {});
-  console.log(` >> session closed: `, response3.data.data);
-
-  clearInterval(timer);
-
-}
-
 app.post('/upload', upload.single('file'), async function (req, res, next) {
   // req.file is the file
   // req.body will hold the text fields, if there were any
@@ -111,9 +46,7 @@ app.post('/upload', upload.single('file'), async function (req, res, next) {
   client.query(
     `INSERT INTO public.file(guid, filename, originalname, filepath, mimetype, filesize) VALUES('${guid}', '${newFileName}', '${file.originalname}', '${newFilePath}', '${file.mimetype}', ${file.size})`,
     (err, res1) => {
-      console.log(err, res1);
-
-      // let p = await doconvert();
+      console.log(` >> INSERT result: `, err, res1);
 
       res.status(200);
       res.end(JSON.stringify({ ok: true, data: { url: `${Settings.public_url}/file/${newFileName}` } }, null, 2));
@@ -130,24 +63,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/file/:filename', async (req, res) => {
-    const file = `./uploads/${req.params.filename}`;
-    res.download(file); // Set disposition and send it.
+  const file = `./uploads/${req.params.filename}`;
+  res.download(file); // Set disposition and send it.
 });
 
 app.listen(Settings.port, () => {
-  console.log(`Example app listening at http://localhost:${Settings.port}`)
+  console.log(`API server is listening at http://localhost:${Settings.port}`)
 });
-
-setInterval(async function () {
-  //const client = await pool.connect();
-  /* client.query("WITH cte AS (SELECT * FROM public.file WHERE status='pending' ORDER BY createdat LIMIT 1) UPDATE job s SET status='running' FROM cte WHERE s.id=cte.id RETURNING s.*", (err, res) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    for (let row of res.rows) {
-      console.log(row);
-    }
-    client.end();
-  }); */
-}, 5000);
